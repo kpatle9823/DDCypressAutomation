4b0c675d176be4bf76db24ec2a8ad5d830022d72
Kamlesh <kamlesh.patle@tresata.com>
2023-07-25 17:19:11 +0530

    Commiting modified changes

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,9 @@
-import './commands'
+import './commands';
 import './common/common_steps';
 
-// Example of a custom Cypress command
-Cypress.Commands.add('login', (username, password) => {
-  // Implement login functionality here
-});
+// // Example of a custom Cypress command
+// Cypress.Commands.add('login', (username, password) => {
+//   // Implement login functionality here
+// });
 
 require('cypress-xpath');
