7816d69ead6fe0096743e81ee16a74a386523533
Kamlesh <kamlesh.patle@tresata.com>
2023-07-26 14:05:51 +0530

    commit homepage

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,9 @@
 import './commands';
-import './common/common_steps';
-
+//import './common/common_steps';
+//Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+   // return false
+  //})
 // // Example of a custom Cypress command
 // Cypress.Commands.add('login', (username, password) => {
 //   // Implement login functionality here
