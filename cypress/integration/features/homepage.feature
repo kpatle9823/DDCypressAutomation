cc03218014c3e246f6ca90b84679af4f7974e4b3
Kamlesh <kamlesh.patle@tresata.com>
2023-07-27 11:02:11 +0530

    only feature

---
+++ cypress/integration/features/homepage.feature
@@ -0,0 +1,10 @@
+Feature: Validate Home page
+
+Background: Login as DE user in Dune appliation
+
+Scenario:  Validate Dune Design home page
+Given Should be able to see tresata walkthrough page
+When User clicks ok got it.
+When user clicks on new Product button.
+And User enters new product name and the description.
+Then user should click on create button
--- cypress/integration/features/login/login.js
+++ cypress/integration/features/login/login.js
@@ -5,15 +5,16 @@ Given("Nevigate to the website", ()=>{
 })
 
  When("User clicks on siginingin button",()=>{
-    cy.wait(10000);
+    //cy.wait(5000);
      cy.xpath('//button[text()="Sign in"]').click();
+     cy.wait(5000);
  })
 
 When("enter {string} and {string}", (username, password)=>{
     //browser.sleep(10000);
     //setDefaultTimeout(60 * 1000);
-    cy.wait(20000)
-    cy.visit("https://koert-test.auth.us-east-1.amazoncognito.com/login?client_id=5v66g6oke90enbmojg6fsg2999&redirect_uri=https%3A%2F%2Fdune-qa.tresata.com%2Fweb%2Fhome&response_type=code&scope=openid&state=b9aacd28d3c1461a8d9bea57dd612c56&code_challenge=BWm1j0DljcFsdNny_9GLwopiaeqQYf469c47sLplR-M&code_challenge_method=S256&response_mode=query");
+   cy.wait(10000)
+    //cy.visit("https://koert-test.auth.us-east-1.amazoncognito.com/login?client_id=5v66g6oke90enbmojg6fsg2999&redirect_uri=https%3A%2F%2Fdune-qa.tresata.com%2Fweb%2Fhome&response_type=code&scope=openid&state=b9aacd28d3c1461a8d9bea57dd612c56&code_challenge=BWm1j0DljcFsdNny_9GLwopiaeqQYf469c47sLplR-M&code_challenge_method=S256&response_mode=query");
     cy.get("#signInFormUsername").clear();
     cy.get("#signInFormUsername").type('${username}{enter}');
     cy.get("#signInFormPassword").clear();
